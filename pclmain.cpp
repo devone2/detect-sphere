8e41b650a4751699045a1b3e36ecb88c6518446c
devon <devon@devon.sk>
2018-09-01 16:53:26 +0200

    Stored non-working

--- pclmain.cpp
+++ pclmain.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+#include<pcl/io/ply_io.h>
+
 int main(int argc, char** argv) {
 
-   std::cout << "Hello from pclmain" << "\n";
+   std::cout << "Loading plt file: " << "\n";
+
+   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ);
+   pcl::PLYReader Reader;
+
+   std::string file("img/PhoFrame(0000).ply");
+   Reader.read(file, *cloud);
+
+   std::cout << "Loaded plt file: " << file <<"\n"
+           //  << "Points in cloud: " << cloud->points.size() << "\n";
 }
